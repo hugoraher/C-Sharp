72356467de2d6bb614edaa2fbb3e0cb2cc82851e
victor hugo <42597105+victorhugoraher@users.noreply.github.com>
2021-02-25 16:13:20 -0600

    Add files via upload
    ¡Importante! 
    
    Cualquier problema de ejecución con el programa, por favor envíame un mensaje a victorhugoraher@gmail.com con los siguientes datos:
    -Fecha
    -Nombre (quien reporta)
    -Asunto 
    -Cuerpo del mensaje
    -Captura de pantalla (sugerido)
    
    Gracias.

---
+++ HorarioDia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HorarioDia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int hora = 24;            //se declara la variable y se le asigna valor
+
+            Console.Write("Ingresa la hora del día en formato de 24 horas, por favor: "); //Se solicita al usuario ingresar datos por el teclado 
+            int hora = Convert.ToInt32(Console.ReadLine());
+
+            if (hora < 6)
+            {             //Inicia la declaración "else if"
+                Console.Write("Son las " + hora + " am," + " es hora de dormir.");
+            }
+            else if (hora < 12)
+            {
+                Console.Write("Son las " + hora + " am," + " buenos días.");
+            }
+            else if (hora < 18)
+            {
+                Console.Write("Son las " + hora + " pm," + " buenas tardes.");
+            }
+            else if (hora < 24)
+            {
+                Console.Write("Son las " + hora + " pm," + " buenas noches.");
+            }
+            else
+            {
+                Console.Write("La hora no existe.");
+            }                       //Termina la declaración "else if"
+        
+        }
+    }
+}
