84d177568fbb8eac736894a69693e0f94772c693
victor hugo <42597105+victorhugoraher@users.noreply.github.com>
2021-03-01 09:31:12 -0600

    Update 02-HorarioDia (Scanner & else-if).cs
    ¡Importante! 
    
    Cualquier problema de ejecución con el programa, por favor envíame un mensaje a victorhugoraher@gmail.com con los siguientes datos:
    -Fecha
    -Nombre (quien reporta)
    -Asunto 
    -Cuerpo del mensaje
    -Captura de pantalla (sugerido)
    
    Gracias.

--- 02-HorarioDia (Scanner & else-if).cs
+++ 02-HorarioDia (Scanner & else-if).cs
@@ -16,7 +16,6 @@ namespace HorarioDia
  * Fecha de creación: 25/02/2021
  * Hora: 06:03 am
  * @author Víctor Hugo Raher
- * @version java 1.8.0_281
  * Fecha última de modificación: 25/02/2021
  * Hora última de modificación: 16:13 pm
  */
